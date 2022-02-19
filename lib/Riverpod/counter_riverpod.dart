a61c420261fbde237dcd81e293771de87ef819f8
Yacine <seurdjimo@gmail.com>
2022-02-19 15:24:15 +0100

    test riverpod

---
+++ lib/Riverpod/counter_riverpod.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CounterNotifier extends ChangeNotifier {
+  int _counter = 0;
+  int get counter => _counter;
+
+  void incrementValue() {
+    _counter++;
+    notifyListeners();
+  }
+}
