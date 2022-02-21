92e87aea90847749b4cfd72104d223f80fc342c8
djamaldev <34842155+djamaldev@users.noreply.github.com>
2022-02-21 10:29:18 +0100

    latest release

--- lib/Riverpod/counter_riverpod.dart
+++ lib/Riverpod/counter_riverpod.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CounterNotifier extends ChangeNotifier {
   int _counter = 0;
   int get counter => _counter;
 
-  void incrementValue() {
+  void incrementValue() async {
     _counter++;
     notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt('counter', _counter);
+  }
+
+  void resetValue() async {
+    _counter = 0;
+    notifyListeners();
+    //final prefs = await SharedPreferences.getInstance();
+    //prefs.setInt('counter', _counter);
+  }
+
+  Future<int?> getIntValue(String key) async {
+    SharedPreferences myPrefs = await SharedPreferences.getInstance();
+    _counter = myPrefs.getInt(key)!;
+    notifyListeners();
+    print(_counter);
+    return _counter;
   }
 }
